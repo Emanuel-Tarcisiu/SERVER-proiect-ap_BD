82b5788970822133e5cd28b7a0c7f4ff7723bc40
Emy <Emy@LAPTOPEMY>
2023-10-29 19:12:12 +0200

    Add project files.

---
+++ CON_BD.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace SERVER
+{
+    internal class CON_BD
+    {
+        private SqlConnection connection;
+
+        //Constructor
+        public CON_BD() {
+            string connectionString = "Data Source=LAPTOPEMY;Initial Catalog=POSproiect;Integrated Security=True";
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+        }
+        //Destructor
+        public void closeConnection()
+        {
+            connection.Close();
+        }
+
+
+        public string Exec_SQL_angajati(string sql)
+        {
+            string columnValue = "";
+
+            SqlCommand command = new SqlCommand(sql, connection);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                columnValue = reader.GetInt32(0).ToString() + "\t" + reader.GetString(1) + "\t" + reader.GetString(2) + "\n";
+            }
+
+            return columnValue;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SERVER
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CON_BD baza_date = new CON_BD();
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPAddress ipAddress = IPAddress.Parse("10.10.24.1");
+            int port = 12345;
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, port);
+
+            listener.Bind(localEndPoint);
+
+            listener.Listen(10);
+
+            Console.WriteLine("Server is listening for connections...");
+
+            while (true)
+            {
+                Socket clientSocket = listener.Accept();
+
+                byte[] buffer = new byte[1024];
+                int bytesRead = clientSocket.Receive(buffer);
+                string receivedMessage = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Received: " + receivedMessage);
+
+                string responseMessage = "Server: Acknowledged - " + receivedMessage;
+
+                byte[] responseBuffer = Encoding.ASCII.GetBytes(responseMessage);
+                clientSocket.Send(responseBuffer);
+
+                clientSocket.Close();
+            }
+
+            //baza_date.closeConnection();
+        }
+    }
+}
